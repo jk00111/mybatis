46013d0cc0532c8fa76d16d1f1150b224c7e316f
jk00111 <kingwinda@gmail.com>
2025-03-31 15:18:53 +0900

    [advice 추가]

---
+++ src/main/java/com/example/mybatis/config/advcie/CustomControllerAdvice.java
@@ -0,0 +1,24 @@
+package com.example.mybatis.config.advcie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class CustomControllerAdvice {
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> illegalArgumentExceptionHandler(IllegalArgumentException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> generalExceptionHandler(RuntimeException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+}
